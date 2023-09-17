a42e9ce6eee4000a420a65bf0ea1521d7909ffa6
karosi12 <adekayor@gmail.com>
2023-09-17 18:38:36 +0100

    retrieve the first 'n' number

--- execrise-3.sh
+++ execrise-3.sh
@@ -4,31 +4,33 @@ read -p "Enter system username: " user
 
 read -p "How do you want to sort the processes memory(m) or CPU(c) (m/c) : " sortBy
 
+read -p "Enter number of lines you want to log/show: " lines
+
 echo "Print the processes running $user"
 
 os="`uname`"
 
-if [ "$sortBy" == "m" ]
+if [ $sortBy == "m" ] || [ $sortBy == "M" ];
   then
   echo "===============Memory=============="
   case $os in 
     'Linux')
-      ps aux --sort -rss | grep $user 
+      ps aux --sort -rss | grep $user | head -n $lines
       ;;
     'Darwin')
-      ps aux | sort -rss | grep $user
+      ps aux | sort -rss | grep $user | head -n $lines
       ;;
     *) echo "unknown: $os" ;;
   esac
-elif [ "$sortBy" == "c" ]
+elif [ $sortBy == "c" ] || [ $sortBy == "C" ];
   then
   echo "===============CPU=============="
   case $os in
     'Linux')
-      ps aux --sort -%cpu | grep $user
+      ps aux --sort -%cpu | grep $user | head -n $lines
       ;;
     'Darwin')
-      ps -eo pcpu,pid,user,args | sort -k 1 -r | grep $user 
+      ps -eo pcpu,pid,user,args | sort -k 1 -r | grep $user | head -n $lines
       ;;
     *) echo "unknown: $os" ;;
   esac
